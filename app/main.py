7364464a69e0865e95dc2bd9f02888ac8b87b4fe
Meet Jethwa <meetjethwa3@gmail.com>
2025-12-24 19:08:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -687,7 +687,7 @@ async def analyze_text(req: AnalyzeTextRequest):
         system=NUTRIENTS_SYSTEM,
         user=nutrients_prompt(nreq),
         temperature=0.2,
-        max_tokens=1600,
+        max_tokens=900,
         search_recency_filter="month",
     )
 
